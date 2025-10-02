947fbeb16ad52de8f260fb1fc1e42bcb7689ed70
LennartTro <lennart.troesken@tum.de>
2025-10-02 17:30:49 +0200

    guided - renamed

--- gz_ws/src/blueboat_guided/setup.py
+++ gz_ws/src/blueboat_guided/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'guid = blueboat_guided.guid:main',
+            'guided = blueboat_guided.guided:main',
         ],
     },
 )
